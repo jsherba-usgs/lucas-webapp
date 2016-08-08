132feaf46d0944fb4e8d09d7cc60f7cc0eca4d81
mukhtyar <shruti.mukhtyar@gmail.com>
2016-08-07 17:41:56 -0700

    Update readme and main page layout

--- src/index.js
+++ src/index.js
@@ -14,40 +14,88 @@ import filters from './components/filters/filters.js';
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Get an array of all links in main nav
-  const nav = document.getElementById('nav');
-  const navLinks = Array.prototype.slice.call(nav.querySelectorAll('a'));
+
 
 
   // Intialize smooth scrolling
   smoothScroll.init({
     updateURL: false,
     easing: 'easeInOutCubic',
-    callback: (anchor, toggle) => {
-      toggle.classList.add('active');
-      navLinks.forEach((link) => {
-        if (link !== toggle) {
-          link.classList.remove('active');
-        }
-      });
-    },
   });
 
-   // Enable responsive menu
-  const headerToggle = document.getElementById('headerToggle');
-  function responsiveMenuToggle(e) {
-    e.preventDefault();
-    document.body.classList.toggle('header-visible');
+ 
+
+ 
+  const body = document.body;
+  const wrapper = document.getElementById('wrapper');
+  const header = document.getElementById('header');
+  const banner = document.getElementById('banner');
+  const overlay = document.getElementById('overlay');
+  const overlayOpenBtn = document.getElementById('overlayOpen');
+  const overlayCloseBtn = document.getElementById('overlayClose');
+
+  // Disable animations/transitions until the page has loaded.
+  body.classList.add('is-loading');
+
+  window.addEventListener('load', function () {
+    window.setTimeout(function () {
+      body.classList.remove('is-loading');
+    }, 100);
+  });
+  window.addEventListener('pageshow', function () {
+    window.setTimeout(function () {
+      body.classList.remove('is-loading');
+    }, 100);
+  });
+
+  // Clear transitioning state on unload/hide.
+  window.addEventListener('unload', function () {
+    window.setTimeout(function () {
+      const transitioningElements = Array.from(document.querySelectorAll('.is-transitioning'));
+      transitioningElements.forEach((el) => el.classList.remove('is-transitioning'));
+    }, 250);
+  });
+  window.addEventListener('pagehide', function () {
+    window.setTimeout(function () {
+      const transitioningElements = Array.from(document.querySelectorAll('.is-transitioning'));
+      transitioningElements.forEach((el) => el.classList.remove('is-transitioning'));
+    }, 250);
+  });
+
+  // Intialize smooth scrolling
+  smoothScroll.init({
+    updateURL: false,
+    easing: 'easeInOutCubic',
+  });
+
+  // Open, close overlay
+  function overlayOpen(e) {
+    body.classList.add('is-overlay-visible');
   }
-  headerToggle.addEventListener('click', responsiveMenuToggle);
-  headerToggle.addEventListener('touchstart', responsiveMenuToggle);
+  function overlayClose(e) {
+    body.classList.remove('is-overlay-visible');
+  }
+  overlayOpenBtn.addEventListener('click', overlayOpen);
+  overlayCloseBtn.addEventListener('click', overlayClose);
+
+
+
+
+
+
+
+
+
+
+
+
 
   // Init filters
   const container = document.getElementById('filters');
   const scenario = '6370';
   filters.init(container);
 
-  console.log(filters.scenario('6370'));
+  //console.log(filters.scenario('6370'));
 
   const params = {
     scenario: '6370',
