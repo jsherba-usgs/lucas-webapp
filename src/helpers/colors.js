79c2ebfe812d4933c1edf3583d24d42d213bd778
Shruti <mukhtyar@berkeley.edu>
2016-08-15 16:00:35 -0700

    Sorting scale values

--- src/helpers/colors.js
+++ src/helpers/colors.js
@@ -1,7 +1,7 @@
 import d3 from 'd3';
 
 export const stateclassColorScale = d3.scale.ordinal()
-  .range(['#a65628', '#4daf4a', '#ff7f00', '#f781bf', '#377eb8', '#e41a1c', '#ffff33',
-    '#999999', '#984ea3'])
-  .domain(['Forest', 'Grassland', 'Shrubland', 'Wetland', 'Water', 'Barren', 'Agriculture',
-    'Developed', 'Plantation']);
+  .range(['#ffff33', '#e41a1c', '#999999', '#a65628',
+    '#4daf4a', '#ff7f00', '#377eb8', '#984ea3', '#f781bf'])
+  .domain(['Agriculture', 'Barren', 'Developed', 'Forest',
+    'Grassland', 'Shrubland', 'Water', 'Plantation', 'Wetland']);
