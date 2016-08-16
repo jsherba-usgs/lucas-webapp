b2be6af433019c57bdbf2538f9ab03deac6709ba
Shruti <mukhtyar@berkeley.edu>
2016-08-16 15:42:55 -0700

    Update landuse colors

--- src/helpers/colors.js
+++ src/helpers/colors.js
@@ -1,7 +1,7 @@
 import d3 from 'd3';
 
 export const stateclassColorScale = d3.scale.ordinal()
-  .range(['#ffff33', '#e41a1c', '#999999', '#a65628',
-    '#4daf4a', '#ff7f00', '#377eb8', '#984ea3', '#f781bf'])
+  .range(['#ffff33', '#e41a1c', '#999999', '#4daf4a',
+    '#ff7f00', '#a65628', '#377eb8', '#984ea3', '#f781bf'])
   .domain(['Agriculture', 'Barren', 'Developed', 'Forest',
-    'Grassland', 'Shrubland', 'Water', 'Plantation', 'Wetland']);
+   'Grassland', 'Shrubland', 'Water', 'Plantation', 'Wetland']);
