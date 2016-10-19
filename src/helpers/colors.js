40378eba8513cb35cec7290ebed53c99b20c92a7
Brian Galey <bgaley@berkeley.edu>
2016-10-19 10:27:52 -0700

    Revise state class color scale based on comments

--- src/helpers/colors.js
+++ src/helpers/colors.js
@@ -1,7 +1,24 @@
 import d3 from 'd3';
 
 export const stateclassColorScale = d3.scale.ordinal()
-  .range(['#ffff33', '#e41a1c', '#999999', '#4daf4a',
-    '#ff7f00', '#a65628', '#377eb8', '#984ea3', '#f781bf'])
-  .domain(['Agriculture', 'Barren', 'Developed', 'Forest',
-   'Grassland', 'Shrubland', 'Water', 'Plantation', 'Wetland']);
+  .range([
+    '#ff8040',
+    '#c0c0c0',
+    '#000',
+    '#008000',
+    '#ffff80',
+    '#a6a600',
+    '#0080ff',
+    '#ff00ff',
+    '#80ffff'
+  ]).domain([
+    'Agriculture',
+    'Barren',
+    'Developed',
+    'Forest',
+    'Grassland',
+    'Shrubland',
+    'Water',
+    'Plantation',
+    'Wetland'
+  ]);
